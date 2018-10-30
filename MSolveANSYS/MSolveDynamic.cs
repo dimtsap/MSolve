4f40a18b48bb75b8812d31ae0a4432510a1b4d6f
Dimitris Tsapetis <dimtsap@hotmail.com>
2018-10-30 11:54:08 +0200

    Adds Dynamic

---
+++ MSolveANSYS/MSolveDynamic.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ansys.ACT.Automation.Mechanical;
+using Ansys.ACT.Automation.Mechanical.AnalysisSettings;
+using Ansys.ACT.Automation.Mechanical.BoundaryConditions;
+using Ansys.ACT.Interfaces.Common;
+using Ansys.ACT.Interfaces.Geometry;
+using Ansys.ACT.Interfaces.Mechanical;
+using Ansys.ACT.Interfaces.Mesh;
+using Ansys.ACT.Interfaces.UserObject;
+using Ansys.EngineeringData.Material;
+using ISAAR.MSolve.Analyzers;
+using ISAAR.MSolve.Discretization.Interfaces;
+using ISAAR.MSolve.FEM.Elements;
+using ISAAR.MSolve.FEM.Entities;
+using ISAAR.MSolve.FEM.Materials;
+using ISAAR.MSolve.Geometry.Shapes;
+using ISAAR.MSolve.Materials;
+using ISAAR.MSolve.Numerical.LinearAlgebra;
+using ISAAR.MSolve.Numerical.LinearAlgebra.Interfaces;
+using ISAAR.MSolve.Problems;
+using ISAAR.MSolve.Solvers.Interfaces;
+using ISAAR.MSolve.Solvers.Skyline;
+using Newtonsoft.Json;
+using Model = ISAAR.MSolve.FEM.Entities.Model;
+
+namespace MSolveANSYS
+{
+	public class MSolveDynamic
+	{
+		private readonly Dictionary<ElementTypeEnum, CellType3D> _ansysMSolveElementDictionary =
+			new Dictionary<ElementTypeEnum, CellType3D>
+			{
+				{ElementTypeEnum.kHex8, CellType3D.Hexa8},
+				{ElementTypeEnum.kHex20, CellType3D.Hexa20},
+				{ElementTypeEnum.kTet4, CellType3D.Tet4},
+				{ElementTypeEnum.kTet10, CellType3D.Tet10},
+				{ElementTypeEnum.kWedge6, CellType3D.Wedge6},
+				{ElementTypeEnum.kWedge15, CellType3D.Wedge15},
+				{ElementTypeEnum.kPyramid5, CellType3D.Pyra5},
+				{ElementTypeEnum.kPyramid13, CellType3D.Pyra13},
+			};
+
+		private readonly Dictionary<ElementTypeEnum, int[]> _ansysMSolveElementLocalCoordinates =
+			new Dictionary<ElementTypeEnum, int[]>()
+			{
+				{ElementTypeEnum.kHex8, new[] {4,5,1,0,7,6,2,3}},
+				{ElementTypeEnum.kHex20, new int[20] {4,5,1,0,7,6,2,3,12,16,15,17,13,8,9,11,14,19,18,10}},
+				{ElementTypeEnum.kTet4, new int[4]{0,3,1,2} },
+				{ElementTypeEnum.kTet10, new int[10]{0,3,1,2,7,8,4,6,5,9} },
+				{ElementTypeEnum.kWedge6, new int[6]{5,4,3,2,1,0} },
+				{ElementTypeEnum.kWedge15, new int[15]{5,4,3,2,1,0,10,11,14,9,13,12,7,8,6} }
+			};
+
+		private readonly IMechanicalExtAPI _api;
+		private readonly IMechanicalUserSolver _solver;
+
+		public MSolveDynamic(IExtAPI api, IUserSolver solver)
+		{
+			_api = api as IMechanicalExtAPI;
+			_solver = solver as IMechanicalUserSolver;
+		}
+
+		public Model model;
+		public IVector SolutionVector;
+		public bool onsolve(IUserSolver userSolver, Func<int, string, bool> func)
+		{
+			var solver = userSolver as IMechanicalUserSolver;
+			try
+			{
+				VectorExtensions.AssignTotalAffinityCount();
+				model = new Model();
+				model.SubdomainsDictionary.Add(0, new Subdomain() { ID = 0 });
+				
+				solver.Analysis.MeshData.Nodes.ToList()
+					.ForEach(n => model.NodesDictionary.Add(n.Id, new Node3D(n.Id, n.X, n.Y, n.Z)));
+
+				var dynamicMaterial= new DynamicMaterial(1,0.05,0.05);
+
+				var material = AnsysUtilities.GetAnsysMaterial(_api);
+				AnsysUtilities.GenerateElements(material, dynamicMaterial, solver, model);
+				AnsysUtilities.CalculateAcceleration(_api,solver,model);
+				//AnsysUtilities.CalculateForces(_api, solver, model);
+				AnsysUtilities.ImposeFixedSupport(_api, solver, model);
+
+				model.ConnectDataStructures();
+
+				var a=_api.Application.InvokeUIThread(()=>_api.DataModel.Project.Model.Analyses[0].PropertyNames) as List<string>;
+
+				var linearSystems = new Dictionary<int, ILinearSystem>();
+				linearSystems[0] = new SkylineLinearSystem(0, model.Subdomains[0].Forces);
+				SolverSkyline solverSkyline = new SolverSkyline(linearSystems[0]);
+				ProblemStructural provider = new ProblemStructural(model, linearSystems);
+				LinearAnalyzer childAnalyzer = new LinearAnalyzer(solverSkyline, linearSystems);
+				NewmarkDynamicAnalyzer parentAnalyzer = new NewmarkDynamicAnalyzer(provider, childAnalyzer, linearSystems, 0.6, 1, 0.02, 53.74);
+				parentAnalyzer.BuildMatrices();
+				parentAnalyzer.Initialize();
+				parentAnalyzer.Solve();
+
+				SolutionVector = linearSystems[0].Solution;
+			}
+			catch (Exception e)
+			{
+				System.IO.File.WriteAllText(@"C:\Users\Dimitris\Desktop\ANSYS Models\error.json", JsonConvert.SerializeObject(e));
+				return false;
+			}
+
+			return true;
+		}
+
+
+
+		
+		public IEnumerable<string> getreader(IUserSolver userSolver)
+		{
+			return new[]
+			{
+				"MSolveANSYS.MSolveStaticReader" ,
+				JsonConvert.SerializeObject(model.NodalDOFsDictionary),
+				JsonConvert.SerializeObject(SolutionVector),
+			};
+		}
+		
+	}
+}
--- MSolveANSYS/MSolveStatic.cs
+++ MSolveANSYS/MSolveStatic.cs
@@ -211,30 +211,7 @@ namespace MSolveANSYS
 				},
 			};
 
-		private readonly Dictionary<ElementTypeEnum, CellType3D> _ansysMSolveElementDictionary =
-			new Dictionary<ElementTypeEnum, CellType3D>
-			{
-				{ElementTypeEnum.kHex8, CellType3D.Hexa8},
-				{ElementTypeEnum.kHex20, CellType3D.Hexa20},
-				{ElementTypeEnum.kTet4, CellType3D.Tet4},
-				{ElementTypeEnum.kTet10, CellType3D.Tet10},
-				{ElementTypeEnum.kWedge6, CellType3D.Wedge6},
-				{ElementTypeEnum.kWedge15, CellType3D.Wedge15},
-				{ElementTypeEnum.kPyramid5, CellType3D.Pyra5},
-				{ElementTypeEnum.kPyramid13, CellType3D.Pyra13},
-			};
-
-		private readonly Dictionary<ElementTypeEnum, int[]> _ansysMSolveElementLocalCoordinates =
-			new Dictionary<ElementTypeEnum, int[]>()
-			{
-				{ElementTypeEnum.kHex8, new[] {4,5,1,0,7,6,2,3}},
-				{ElementTypeEnum.kHex20, new int[20] {4,5,1,0,7,6,2,3,12,16,15,17,13,8,9,11,14,19,18,10}},
-				{ElementTypeEnum.kTet4, new int[4]{0,3,1,2} },
-				{ElementTypeEnum.kTet10, new int[10]{0,3,1,2,7,8,4,6,5,9} },
-				{ElementTypeEnum.kWedge6, new int[6]{5,4,3,2,1,0} },
-				{ElementTypeEnum.kWedge15, new int[15]{5,4,3,2,1,0,10,11,14,9,13,12,7,8,6} }
-			};
-
+		
 		private readonly IMechanicalExtAPI _api;
 		private readonly IMechanicalUserSolver _solver;
 
@@ -259,13 +236,10 @@ namespace MSolveANSYS
 				solver.Analysis.MeshData.Nodes.ToList()
 					.ForEach(n => model.NodesDictionary.Add(n.Id, new Node3D(n.Id, n.X, n.Y, n.Z)));
 				
-				var material = GetAnsysMaterial();
-
-				GenerateElements(material, solver);
-
-				CalculateForces(solver);
-
-				ImposeFixedSupport(solver);
+				var material =AnsysUtilities.GetAnsysMaterial(_api);
+				AnsysUtilities.GenerateElements(material, null, solver, model);
+				AnsysUtilities.CalculateForces(_api,solver,model);
+				AnsysUtilities.ImposeFixedSupport(_api,solver,model);
 
 				model.ConnectDataStructures();
 				var linearSystems = new Dictionary<int, ILinearSystem>();
@@ -289,183 +263,7 @@ namespace MSolveANSYS
 			return true;
 		}
 
-		private void ImposeFixedSupport(IMechanicalUserSolver solver)
-		{
-			var ansysFixedSupports =
-				_api.Application.InvokeUIThread(() => _api.DataModel.Project.Model.Analyses[0].Children
-						.Where(c => c.GetType() == typeof(FixedSupport)).ToList()) as List<DataModelObject>;
-
-			foreach (var ansysFixedSupport in ansysFixedSupports)
-			{
-				var fixedSupport = ansysFixedSupport as FixedSupport;
-				var fixedLocation = _api.Application.InvokeUIThread(() => fixedSupport.Location) as ISelectionInfo;
-				var fixedSurfaceId = fixedLocation.Ids[0];
-				var fixedNodes = solver.Analysis.MeshData.MeshRegionById(fixedSurfaceId).Nodes;
-
-				foreach (var node in fixedNodes)
-				{
-					model.NodesDictionary[node.Id].Constraints.Add(DOFType.X);
-					model.NodesDictionary[node.Id].Constraints.Add(DOFType.Y);
-					model.NodesDictionary[node.Id].Constraints.Add(DOFType.Z);
-				}
-			}
-		}
-
-		private void GenerateElements(ElasticMaterial3D material, IMechanicalUserSolver solver)
-		{
-			var factory = new ContinuumElement3DFactory(material, null);
-			foreach (var ansysElement in solver.Analysis.MeshData.Elements)
-			{
-				var ansysNodes = new List<Node3D>();
-				ansysElement.NodeIds.ToList().ForEach(id => ansysNodes.Add((Node3D) model.NodesDictionary[id]));
-				var msolveNodes = RenumberNodesFromAnsysToMSolve(ansysNodes, ansysElement.Type);
-				var element = factory.CreateElement(_ansysMSolveElementDictionary[ansysElement.Type], msolveNodes);
-				var elementWrapper = new Element() {ID = ansysElement.Id, ElementType = element};
-				foreach (var node in element.Nodes) elementWrapper.AddNode(node);
-				model.ElementsDictionary.Add(ansysElement.Id, elementWrapper);
-				model.SubdomainsDictionary[0].ElementsDictionary.Add(ansysElement.Id, elementWrapper);
-			}
-		}
-
-		private ElasticMaterial3D GetAnsysMaterial()
-		{
-			var mat = (_api.DataModel.GeoData.Assemblies[0].Parts[0].Bodies[0] as IGeoBody).Material as MaterialClass;
-			var dataDictionary = GetMaterialPropertyByName(mat, "Elasticity");
-			var material = new ElasticMaterial3D()
-			{
-				YoungModulus = dataDictionary["Young's Modulus"].Value,
-				PoissonRatio = dataDictionary["Poisson's Ratio"].Value
-			};
-			return material;
-		}
-
-		private void CalculateForces(IMechanicalUserSolver solver)
-		{
-			var forces =
-				_api.Application.InvokeUIThread(() => _api.DataModel.Project.Model.Analyses[0].Children
-					.Where(c => c.GetType() == typeof(Force)).ToList()) as List<DataModelObject>;
-
-			foreach (var ansysForce in forces)
-			{
-				var force = ansysForce as Force;
-				var isParsed = Enum.TryParse<LoadDefineBy>(_api.Application.InvokeUIThread(() => force.DefineBy).ToString(), out var defineBy);
-				if (defineBy==LoadDefineBy.Vector)
-				{
-					CalculateVectorForce(solver, force);
-				}
-				else
-				{
-					CalculateComponentsForce(solver, force);
-				}
-			}
-		}
-
-		private void CalculateComponentsForce(IMechanicalUserSolver solver, Force force)
-		{
-			var forceLocation = _api.Application.InvokeUIThread(() => force.Location) as ISelectionInfo;
-			var xValue = _api.Application.InvokeUIThread(() => force.XComponent.Output.DiscreteValues[1]) as Quantity;
-			var yValue = _api.Application.InvokeUIThread(() => force.YComponent.Output.DiscreteValues[1]) as Quantity;
-			var zValue = _api.Application.InvokeUIThread(() => force.ZComponent.Output.DiscreteValues[1]) as Quantity;
-			var forceSurfaceId = forceLocation.Ids[0];
-			var forceNodes = solver.Analysis.MeshData.MeshRegionById(forceSurfaceId).Nodes;
-
-			foreach (var node in forceNodes)
-			{
-				model.Loads.Add(new Load()
-				{
-					Amount = xValue.Value / forceNodes.Count,
-					Node = model.NodesDictionary[node.Id],
-					DOF = DOFType.X
-				});
-				model.Loads.Add(new Load()
-				{
-					Amount = yValue.Value / forceNodes.Count,
-					Node = model.NodesDictionary[node.Id],
-					DOF = DOFType.Y
-				});
-				model.Loads.Add(new Load()
-				{
-					Amount = zValue.Value / forceNodes.Count,
-					Node = model.NodesDictionary[node.Id],
-					DOF = DOFType.Z
-				});
-			}
-		}
-
-		private void CalculateVectorForce(IMechanicalUserSolver solver, Force force)
-		{
-			var forceLocation = _api.Application.InvokeUIThread(() => force.Location) as ISelectionInfo;
-			var xValue = _api.Application.InvokeUIThread(() => force.XComponent.Output.DiscreteValues[0]) as Quantity;
-			var yValue = _api.Application.InvokeUIThread(() => force.YComponent.Output.DiscreteValues[0]) as Quantity;
-			var zValue = _api.Application.InvokeUIThread(() => force.ZComponent.Output.DiscreteValues[0]) as Quantity;
-			var magnitude = _api.Application.InvokeUIThread(() => force.Magnitude.Output.DiscreteValues[1]) as Quantity;
-			var forceSurfaceId = forceLocation.Ids[0];
-			var forceNodes = solver.Analysis.MeshData.MeshRegionById(forceSurfaceId).Nodes;
-
-			foreach (var node in forceNodes)
-			{
-				model.Loads.Add(new Load()
-				{
-					Amount = xValue.Value * magnitude.Value / forceNodes.Count,
-					Node = model.NodesDictionary[node.Id],
-					DOF = DOFType.X
-				});
-				model.Loads.Add(new Load()
-				{
-					Amount = yValue.Value * magnitude.Value / forceNodes.Count,
-					Node = model.NodesDictionary[node.Id],
-					DOF = DOFType.Y
-				});
-				model.Loads.Add(new Load()
-				{
-					Amount = zValue.Value * magnitude.Value / forceNodes.Count,
-					Node = model.NodesDictionary[node.Id],
-					DOF = DOFType.Z
-				});
-			}
-		}
-
-		private List<Node3D> RenumberNodesFromAnsysToMSolve(IReadOnlyList<Node3D> ansysNodes, ElementTypeEnum elementType)
-		{
-			var connectivity = _ansysMSolveElementLocalCoordinates[elementType];
-			return ansysNodes.Select((t, i) => ansysNodes[connectivity[i]]).ToList();
-		}
-
-		/// <summary>
-		/// Return the list of material property names for the given material.
-		/// </summary>
-		/// <param name="material"></param>
-		/// <returns></returns>
-		private List<string> GetListMaterialProperties(MaterialClass material)
-		{
-			var propertyNames = new List<string>();
-			for (int i = 0; i < material.MaterialProperties.Count; i++)
-			{
-				propertyNames.Add(material.MaterialProperties.Get(i).TypeName);
-			}
-
-			return propertyNames;
-		}
-
-		private Dictionary<string, (string Unit, double Value)> GetMaterialPropertyByName(MaterialClass material, string propertyName)
-		{
-			var properties = new List<IMaterialProperty>();
-			for (var i = 0; i < material.MaterialProperties.Count; i++)
-			{
-				properties.Add(material.MaterialProperties.Get(i));
-			}
-
-			var property= properties.Single(p => p.TypeName == propertyName);
-			
-			var variablesDictionary=new Dictionary<string, (string unit ,double value)>();
-			for (int i = 0; i < property.MaterialPropertyDatas.Get(0).Variables.Count; i++)
-			{
-				var variable = property.MaterialPropertyDatas.Get(0).Variables.Get(i);
-				variablesDictionary.Add(variable.TypeName, (variable.Unit,variable.DatumColl.Get(0).Value));
-			}
-
-			return variablesDictionary;
-		}
+		
 
 		public IEnumerable<string> getreader(IUserSolver userSolver)
 		{

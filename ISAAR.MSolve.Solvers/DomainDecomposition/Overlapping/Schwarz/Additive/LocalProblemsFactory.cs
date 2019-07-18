dec8f575698b8550e5a6b0e6c2eedce8b9bbc5d4
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-07-18 11:52:01 +0300

    Implements LocalProblems factory

--- ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/LocalProblemsFactory.cs
+++ ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/LocalProblemsFactory.cs
@@ -2,19 +2,50 @@
 using System.Collections.Generic;
 using System.Text;
 using ISAAR.MSolve.LinearAlgebra.Matrices;
+using ISAAR.MSolve.LinearAlgebra.Matrices.Builders;
+using ISAAR.MSolve.LinearAlgebra.Triangulation;
+using ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.Interfaces;
 
 namespace ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive
 {
     public class LocalProblemsFactory
     {
-        internal void GenerateMatrices(IOverlappingDecomposition overlappingDecomposition)
+        private Matrix[] inverseMatrices;
+        private IModelOverlappingDecomposition _modelOverlappingDecomposition;
+        private int _matrixOrder;
+
+        public void GenerateMatrices(IMatrixView matrix, IModelOverlappingDecomposition modelOverlappingDecomposition)
         {
-            throw new NotImplementedException();
+            _modelOverlappingDecomposition = modelOverlappingDecomposition;
+            _matrixOrder = matrix.NumRows;
+            inverseMatrices = new Matrix[modelOverlappingDecomposition.NumberOfSubdomains];
+
+            for (int i = 0; i < modelOverlappingDecomposition.NumberOfSubdomains; i++)
+            {
+                var subdomainConnectivity = modelOverlappingDecomposition.GetConnectivityOfSubdomain(i);
+                var submatrix=matrix.GetSubmatrix(subdomainConnectivity, subdomainConnectivity).CopyToFullMatrix();
+                inverseMatrices[i]= submatrix.Invert();
+            }
         }
 
-        public CsrMatrix RetrievePreconditionerContribution()
+        public Matrix RetrievePreconditionerContribution()
         {
-            throw new NotImplementedException();
+            var localProblemsContribution =Matrix.CreateZero(_matrixOrder, _matrixOrder);
+            for (int i = 0; i < _modelOverlappingDecomposition.NumberOfSubdomains; i++)
+            {
+                var subdomainConnectivity = _modelOverlappingDecomposition.GetConnectivityOfSubdomain(i);
+                var inverseMatrix = inverseMatrices[i];
+                for (int j = 0; j < subdomainConnectivity.Length; j++)
+                {
+                    for (int k = 0; k < subdomainConnectivity.Length; k++)
+                    {
+                        localProblemsContribution[subdomainConnectivity[j], subdomainConnectivity[k]] +=
+                            inverseMatrix[j, k];
+                    }
+                }
+            }
+
+            return localProblemsContribution;
         }
     }
 }

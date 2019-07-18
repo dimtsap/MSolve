2eef7cc7e8ec6fcdc55a160813e2475930e54eb5
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-07-18 11:43:45 +0300

    Adds interface for coarse problem factory

--- ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/CoarseProblem/ICoarseProblemFactory.cs
+++ ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/CoarseProblem/ICoarseProblemFactory.cs
@@ -1,10 +1,13 @@
-﻿using ISAAR.MSolve.LinearAlgebra.Matrices;
+﻿using System.Collections.Generic;
+using ISAAR.MSolve.Geometry.Coordinates;
+using ISAAR.MSolve.LinearAlgebra.Matrices;
+using ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.Interfaces;
 
 namespace ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.CoarseProblem
 {
     public interface ICoarseProblemFactory
     {
-        void GenerateMatrices(IOverlappingDecomposition overlappingDecomposition);
-        CsrMatrix RetrievePreconditionerContribution();
+        void GenerateMatrices(IMatrixView matrix,IModelOverlappingDecomposition modelOverlappingDecomposition);
+        Matrix RetrievePreconditionerContribution();
     }
 }

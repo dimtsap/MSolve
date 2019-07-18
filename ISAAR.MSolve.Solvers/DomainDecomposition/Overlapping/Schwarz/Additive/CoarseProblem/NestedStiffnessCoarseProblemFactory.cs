b803396db4369f346e87a75013823fc4b56836d9
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-07-18 11:45:07 +0300

    Adds unimplemented nested stiffness and non-nested interpolation coarse spaces

--- ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/CoarseProblem/NestedStiffnessCoarseProblemFactory.cs
+++ ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/CoarseProblem/NestedStiffnessCoarseProblemFactory.cs
@@ -1,10 +1,13 @@
-﻿using ISAAR.MSolve.LinearAlgebra.Matrices;
+﻿using System.Collections.Generic;
+using ISAAR.MSolve.Geometry.Coordinates;
+using ISAAR.MSolve.LinearAlgebra.Matrices;
+using ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.Interfaces;
 
 namespace ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.CoarseProblem
 {
     public class NestedStiffnessCoarseProblemFactory : ICoarseProblemFactory
     {
-        public void GenerateMatrices(IOverlappingDecomposition overlappingDecomposition)
+        public void GenerateMatrices(IMatrixView matrix, IModelOverlappingDecomposition modelOverlappingDecomposition)
         {
             throw new System.NotImplementedException();
         }
--- ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/CoarseProblem/NonNestedInterpolationCoarseProblemFactory.cs
+++ ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/CoarseProblem/NonNestedInterpolationCoarseProblemFactory.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ISAAR.MSolve.Geometry.Coordinates;
 using ISAAR.MSolve.LinearAlgebra.Matrices;
+using ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.Interfaces;
 
 namespace ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.CoarseProblem
 {
     public class NonNestedInterpolationCoarseProblemFactory : ICoarseProblemFactory
     {
-        public void GenerateMatrices(IOverlappingDecomposition overlappingDecomposition)
+        public void GenerateMatrices(IMatrixView matrix, IModelOverlappingDecomposition modelOverlappingDecomposition)
         {
             throw new NotImplementedException();
         }

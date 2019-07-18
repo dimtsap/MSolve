2d11e7a655fc55dea4854299c97c66e80a352493
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-07-18 11:46:19 +0300

    Adds parametrix axis decomposition implementation

--- ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/ParametricAxisDecomposition.cs
+++ ISAAR.MSolve.Solvers/DomainDecomposition/Overlapping/Schwarz/Additive/ParametricAxisDecomposition.cs
@@ -3,54 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ISAAR.MSolve.LinearAlgebra.Vectors;
+using ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive.Interfaces;
 
 namespace ISAAR.MSolve.Solvers.DomainDecomposition.Overlapping.Schwarz.Additive
 {
-    public class ParametricAxisDecomposition:IOverlappingDecomposition
+    public class ParametricAxisDecomposition:IAxisOverlappingDecomposition
     {
         private List<double> _decompositionIndices;
-        public ParametricAxisDecomposition(IVector knotValueVector,int degree, int numberOfProcessors)
+        private int[][] _subdomainIndices;
+        private IVector _knotValueVector;
+        private int _numberOfSubdomains;
+        private int _degree;
+        private double[] _coarsePoints;
+        private double[] _coarseKnotVector;
+
+        public ParametricAxisDecomposition(IVector knotValueVector,int degree, int numberOfSubdomains)
         {
-            var numberOfShapeFunctions = knotValueVector.Length - degree - 1;
-            var shapeFunctionSupports= new List<FunctionSupport>();
+            _knotValueVector = knotValueVector;
+            _numberOfSubdomains = numberOfSubdomains;
+            _degree = degree;
+        }
+
+        public int NumberOfAxisSubdomains => _subdomainIndices.GetLength(0);
+
+        public int Degree => _degree;
+
+        public IVector KnotValueVector => _knotValueVector;
+
+        public void DecomposeAxis()
+        {
+            var numberOfShapeFunctions = _knotValueVector.Length - _degree - 1;
+            var shapeFunctionSupports = new List<FunctionSupport>();
             var indexFunction = 0;
             for (int i = 0; i < numberOfShapeFunctions; i++)
             {
                 shapeFunctionSupports.Add(new FunctionSupport
                 {
                     ID = indexFunction++,
-                    Start = knotValueVector[i],
-                    End = knotValueVector[i+degree+1]
+                    Start = _knotValueVector[i],
+                    End = _knotValueVector[i + _degree + 1]
                 });
             }
 
-            var knots = knotValueVector.CopyToArray().Distinct().ToList();
+            var knots = _knotValueVector.CopyToArray().Distinct().ToList();
             var axisElements = knots.Count() - 1;
-            var subdomainElements = (axisElements % numberOfProcessors == 0)
-                ? axisElements / numberOfProcessors
-                : axisElements / numberOfProcessors + 1;
+            var subdomainElements = (axisElements % _numberOfSubdomains == 0)
+                ? axisElements / _numberOfSubdomains
+                : axisElements / _numberOfSubdomains + 1;
 
             _decompositionIndices = new List<double>();
-            for (int i = 0; i < knots.Count(); i+=subdomainElements)
+            for (int i = 0; i < knots.Count() - 1; i += subdomainElements)
                 _decompositionIndices.Add(knots[i]);
-            _decompositionIndices.Add(knots[knots.Count-1]);
+            _decompositionIndices.Add(knots[knots.Count - 1]);
 
-            var shapeFunctionsIds=new List<int>();
-            shapeFunctionsIds.Add(0);
-            foreach (var index in _decompositionIndices)
-            {
-                var id = shapeFunctionSupports.First(s => s.Start < index && s.End > index).ID;
-                shapeFunctionsIds.Add(id);
-            }
+            var coarseKnotVector = new List<double>();
+            for (int i = 0; i < _degree; i++)
+                coarseKnotVector.Add(knots[0]);
+            coarseKnotVector.AddRange(_decompositionIndices);
+            for (int i = 0; i < _degree; i++)
+                coarseKnotVector.Add(knots[knots.Count-1]);
 
-            var subdomainIndices = new int[shapeFunctionsIds.Count-1][];
-            for (int i = 0; i < shapeFunctionsIds.Count-2; i++)
+            _coarseKnotVector = coarseKnotVector.ToArray();
+
+            var numberOfCoarsePoints = coarseKnotVector.Count - _degree - 1;
+            _coarsePoints=new double[numberOfCoarsePoints];
+            for (int i = 0; i < numberOfCoarsePoints; i++)
             {
-                var indexCount = shapeFunctionsIds[i + 1] - shapeFunctionsIds[i];
-                subdomainIndices[i] = Enumerable.Range(shapeFunctionsIds[i], indexCount).ToArray();
+                var sum = 0.0;
+                for (int j = i+1; j <= i+_degree; j++)
+                {
+                    sum += coarseKnotVector[j];
+                }
+                _coarsePoints[i]=sum/_degree;
             }
             
+
+            var shapeFunctionsIds = new List<int>();
+            shapeFunctionsIds.Add(0);
+            for (int i = 1; i < _decompositionIndices.Count - 1; i++)
+            {
+                var index = _decompositionIndices[i];
+                var functions = shapeFunctionSupports.Where(s => s.Start < index && s.End > index).ToArray();
+                var j = functions.Count() % 2 != 0 ? functions.Count() / 2 : functions.Count() / 2 - 1;
+                var id = functions[j].ID;
+                shapeFunctionsIds.Add(id);
+            }
+            shapeFunctionsIds.Add(numberOfShapeFunctions);
+
+            _subdomainIndices = new int[shapeFunctionsIds.Count - 1][];
+            for (int i = 0; i < shapeFunctionsIds.Count - 1; i++)
+            {
+                var indexCount = (shapeFunctionsIds[i] == 0)
+                    ? shapeFunctionsIds[i + 1] - shapeFunctionsIds[i] + 1
+                    : shapeFunctionsIds[i + 1] - shapeFunctionsIds[i];
+                _subdomainIndices[i] = Enumerable.Range(shapeFunctionsIds[i], indexCount).ToArray();
+            }
         }
+
+        public double[] GetAxisCoarseKnotVector() => _decompositionIndices.ToArray();
+
+        public double[] GetAxisCoarsePoints() => _coarsePoints;
+
+        public int[] GetAxisIndicesOfSubdomain(int indexAxisSubdomain) => _subdomainIndices[indexAxisSubdomain];
     }
 
     public class FunctionSupport

018ea86427331fd075ae278cf5c0f8d63129abf9
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-07-20 17:21:31 +0300

    Moves weighted point to discretization

---
+++ ISAAR.MSolve.Discretization/Interfaces/IWeightedPoint.cs
@@ -0,0 +1,7 @@
+﻿namespace ISAAR.MSolve.Discretization.Interfaces
+{
+    public interface IWeightedPoint:INode
+    {
+        double WeightFactor { get; set; }
+    }
+}
--- ISAAR.MSolve.Geometry/Coordinates/IWeightedPoint.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ISAAR.MSolve.Geometry.Coordinates
-{
-    public interface IWeightedPoint
-    {
-        int ID { get; set; }
-        double WeightFactor { get; set; }
-    }
-}

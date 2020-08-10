5c7fa2afe2e47261a405918627a637a8b75030ae
Dimitris Tsapetis <dimtsap@hotmail.com>
2020-08-10 12:33:53 +0300

    Adds SurfaceLoadElementFactory

--- ISAAR.MSolve.IGA/Loading/LoadElementFactories/SurfaceLoadElementFactory.cs
+++ ISAAR.MSolve.IGA/Loading/LoadElementFactories/SurfaceLoadElementFactory.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ISAAR.MSolve.IGA.Entities;
+using ISAAR.MSolve.IGA.Loading.Interfaces;
+using ISAAR.MSolve.IGA.Readers.NurbsMesh;
 
 namespace ISAAR.MSolve.IGA.Loading.LoadElementFactories
 {
-    class SurfaceLoadElementFactory
+    public enum NurbsSolidFaces
     {
+        Left,
+        Right,
+        Bottom,
+        Top,
+        Front,
+        Back
+    }
+
+    public class SurfaceLoadElementFactory
+    {
+        private readonly ISurfaceLoad load;
+        private readonly Model model;
+
+        public SurfaceLoadElementFactory(ISurfaceLoad load, Model model)
+        {
+            this.load = load;
+            this.model = model;
+        }
+
+        public List<SurfaceLoadElementFactory> CreateElements(NurbsSolidGeometryDto solidGeometry, NurbsSolidFaces face)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SurfaceLoadElementFactory> CreateElements(NurbsSurfaceGeometryDto surfaceGeometry)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
